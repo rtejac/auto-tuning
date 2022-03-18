00034ae62fc867de61fbedea917779606bb9b7e9
rtejac <radhakrishna.teja.c@intel.com>
2022-03-18 14:26:44 +0530

    Auto tuning initial commit to GitHub

---
+++ web_console/templates/telusko/calc/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('',views.home,name='home'),   #  '/' or ' ' is referred as home
+    path('optimize',views.optimize,name='optimize'),
+    path('download',views.download,name='download'),
+    path('repeat',views.home,name='repeat'),
+    path('hadfile',views.hadfile,name='hadfile'),
+    path('apply',views.apply,name='apply'),
+    path('view_all',views.view_all,name='view_al'),
+
+]
